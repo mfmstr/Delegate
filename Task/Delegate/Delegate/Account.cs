af75f0b996e48dc15fd763993172f2dcb5922198
mfmstr <g.kharebava01@gmail.com>
2023-09-07 03:44:44 +0400

    Delegate with annonimous methods and lambda

---
+++ Task/Delegate/Delegate/Account.cs
@@ -0,0 +1,47 @@
+﻿namespace Delegate
+{
+    delegate void NotificationDelegate(string message);
+    class Account
+    {
+
+        NotificationDelegate notificationDelegate;
+
+        public decimal Balance;
+
+        public void setNotificationDelegate(NotificationDelegate notificationDelegate)
+        {
+            this.notificationDelegate += notificationDelegate;
+        }
+
+        public Account(decimal balance)
+        {
+            this.Balance = balance;
+        }
+        public void fillBalance(decimal balance)
+        {
+            this.Balance += balance;
+            if (notificationDelegate != null)
+            {
+                notificationDelegate($"Your balance is increased by {balance} GEL. Current balance is: {this.Balance}");
+            }
+        }
+        public void withdrow(decimal balance)
+        {
+            if (this.Balance >= balance)
+            {
+                this.Balance -= balance;
+                if (notificationDelegate != null)
+                {
+                    notificationDelegate($"Withdrown {balance} GEL from your account. Current balance is: {this.Balance}");
+                }
+            }
+            else
+            {
+                if (notificationDelegate != null)
+                {
+                    notificationDelegate("Not enough balance");
+                }
+            }
+        }
+    }
+}
---
+++ Task/Delegate/Delegate/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace Delegate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Account account1 = new Account(500);
+            // Delegate for console printing
+            account1.setNotificationDelegate(message => Console.WriteLine(message));
+            // Delegate for writing in file
+            account1.setNotificationDelegate(message =>
+            {
+                string path = @"C:\Users\User\Desktop\Delegate\Log.txt";
+                File.AppendAllText(path, message + Environment.NewLine);
+            });
+
+            account1.fillBalance(3000);
+            account1.withdrow(100);
+        }
+    }
+}
